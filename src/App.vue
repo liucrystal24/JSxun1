61228b2ed9e12dfb98f65aeb2473a796398a36a4
liucrystal24 <liucrystal_24@outlook.com>
2020-03-24 10:46:47 +0800

    JSxunv0.1

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="header">
+        <div class="logo">
+          <img src="./assets/logo1.png" />
+          <div class="pcompany">中国气象科学研究院</div>
+        </div>
+        <div class="headerright">
+          <div class="projectname">人工增雨随机化外场试验和效果检验技术研究(数据库)</div>
+          <div class="headernav">
+            <div class="navcontent" @click="navchange1">
+              <router-link :to="{name:'n1c2'}" class="nava" :class="{nav_active:navshow == 1}">福建省</router-link>
+            </div>
+            <div class="navcontent" @click="navchange2">
+              <router-link :to="{name:'n2c1'}" class="nava" :class="{nav_active:navshow == 2}">海南省</router-link>
+            </div>
+            <div class="navcontent" @click="navchange3">
+              <router-link :to="{name:'n3c1'}" class="nava" :class="{nav_active:navshow == 3}">吉林省</router-link>
+            </div>
+            <div class="navcontent" @click="navchange4">
+              <router-link :to="{name:'n4c1'}" class="nava" :class="{nav_active:navshow == 4}">山东省</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="projectname">人工增雨随机化试验样本数据库</div> -->
+    </header>
+
+    <router-view class="navchildren" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      navshow: 1
+    };
+  },
+  methods: {
+    navchange1() {
+      this.navshow = 1;
+    },
+    navchange2() {
+      this.navshow = 2;
+    },
+    navchange3() {
+      this.navshow = 3;
+    },
+    navchange4() {
+      this.navshow = 4;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app {
+  height: 100%;
+  min-height: 608px;
+  min-width: 1080px;
+}
+.logo {
+  width: 205px;
+  height: 150px;
+  overflow: hidden;
+  background-color: #2958DE;
+  padding-top: 10px;
+  float: left;
+}
+.logo img {
+  width: 100px;
+  line-height: 100px;
+  /* height: 100px; */
+}
+.pcompany {
+  width: 100%;
+  line-height: 50px;
+  height: 50px;
+  font-size: 18px;
+  color: #fff;
+}
+.projectname {
+  float: left;
+  position: relative;
+  left: 90px;
+  line-height: 100px;
+  font-weight: bolder;
+  font-size: 32px;
+}
+
+header {
+  width: 100%;
+  height: 150px;
+  background-color: #e8eaf2;
+  float: left;
+  color: #273c9b;
+
+  /* line-height: 100px; */
+}
+.header {
+  float: left;
+  height: 150px;
+  width: 100%;
+}
+.headerright {
+  width: 100%;
+  padding-left: 205px;
+  height: 150px;
+  box-sizing: border-box;
+}
+.headernav {
+  /* padding-left:170px; */
+  width: 100%;
+  /* margin-left: 170px; */
+  /* height: 50px; */
+  background-color: #324098;
+  float: left;
+}
+.navcontent {
+  float: left;
+  width: 25%;
+  height: 100%;
+  line-height: 50px;
+  box-sizing: border-box;
+}
+.nava {
+  height: 100%;
+  width: 100%;
+  display: block;
+  font-size: 15px;
+  text-decoration: none;
+  color: #fff;
+  background-color: #1d6ec7;
+  border-right: 1px solid #324098;
+  box-sizing: border-box;
+}
+.nav_active {
+  background-color: #324098 !important;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.navchildren {
+  height: 100%;
+  /* position: absolute; */
+  padding-top: 150px;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Nav1.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="hello">
+    <div class="leftnav">
+      <div class="lefttitle">
+        <i class="el-icon-s-data"></i>
+        数据库列表
+      </div>
+      <!-- <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#6995EB"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        unique-opened="true"
+        router="true"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span style='fontSize:18px;'>福建</span>
+          </template>
+          <el-menu-item index="n1c4">选项1</el-menu-item>
+          <el-menu-item index="n1c1">选项2</el-menu-item>
+          <el-menu-item index="n1c5">选项3</el-menu-item>
+          <el-menu-item index="n1c3">选项4</el-menu-item>
+          <el-menu-item index="n1c6">选项4</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>海南</span>
+          </template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-menu-item index="2-4">选项4</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>吉林</span>
+          </template>
+          <el-menu-item index="3-1">选项1</el-menu-item>
+          <el-menu-item index="3-2">选项2</el-menu-item>
+          <el-menu-item index="3-3">选项3</el-menu-item>
+          <el-menu-item index="3-4">选项4</el-menu-item>
+        </el-submenu>
+        <el-submenu index="4">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>山东</span>
+          </template>
+          <el-menu-item index="4-1">选项1</el-menu-item>
+          <el-menu-item index="4-2">选项2</el-menu-item>
+          <el-menu-item index="4-3">选项3</el-menu-item>
+          <el-menu-item index="4-4">选项4</el-menu-item>
+        </el-submenu>
+      </el-menu> -->
+      <div :class="{ leftactive: leftnum == 2 }" class="leftnavitem" @click="leftnav2">
+        <router-link :to="{ name: 'n1c4' }" class="lefta">地图总览</router-link>
+      </div>
+      <div :class="{ leftactive: leftnum == 5 }" @click="leftnav5" class="leftnavitem">
+        <router-link :to="{ name: 'n1c5' }" class="lefta">探空</router-link>
+      </div>
+      <div :class="{ leftactive: leftnum == 1 }" class="leftnavitem" @click="leftnav1">
+        <router-link :to="{ name: 'n1c1' }" class="lefta">雷达</router-link>
+      </div>
+      <div :class="{ leftactive: leftnum == 3 }" class="leftnavitem" @click="leftnav3">
+        <router-link :to="{ name: 'n1c3' }" class="lefta">雨滴谱</router-link>
+      </div>
+      <div :class="{ leftactive: leftnum == 4 }" class="leftnavitem" @click="leftnav4">
+        <router-link :to="{ name: 'n1c6' }" class="lefta">自动站</router-link>
+      </div>
+      <div class="footer">
+        <div class="comlogo">
+          <img src="../assets/comlogo.png" />
+        </div>
+        <div class="leftcompany">南京云卫通软件技术有限公司</div>
+      </div>
+    </div>
+    <router-view class="rightchild"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav1",
+  data() {
+    return {
+      leftnum: 2
+    };
+  },
+  methods: {
+    leftnav1() {
+      this.leftnum = 1;
+    },
+    leftnav2() {
+      this.leftnum = 2;
+    },
+    leftnav3() {
+      this.leftnum = 3;
+    },
+    leftnav4() {
+      this.leftnum = 4;
+    },
+    leftnav5() {
+      this.leftnum = 5;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #fff;
+  /* height: 100px;
+  display: block; */
+}
+.hello {
+  height: 100%;
+}
+.lefttitle {
+  color: #fff;
+  height: 40px;
+  text-align: left;
+  padding-left: 15px;
+  padding-top: 20px;
+  background-color: #409eff;
+}
+.leftnav {
+  height: 100%;
+  width: 205px;
+
+  /* min-height: 100%; */
+  display: flex;
+  flex-direction: column;
+
+  background-color: cornflowerblue;
+  float: left;
+}
+
+.leftnavitem {
+  height: 40px;
+  line-height: 40px;
+}
+.lefta {
+  height: 100%;
+  width: 100%;
+  display: block;
+  text-decoration: none;
+}
+.leftactive {
+  background-color: #324098;
+}
+.footer {
+  height: 120px;
+  margin-top: auto;
+}
+.comlogo {
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
+  overflow: hidden;
+  margin-left: 45px;
+  /* position: absolute; */
+  /* bottom: 55px; */
+  /* left: 45px; */
+}
+.comlogo img {
+  widows: 100%;
+  height: 100%;
+  position: relative;
+  /* left: -15px; */
+  /* position: absolute;
+  left: 5px; */
+}
+.leftcompany {
+  width: 170px;
+  color: #fff;
+  font-size: 12px;
+  margin-top: 5px;
+  /* position: absolute; */
+  /* bottom: 30px; */
+}
+.rightchild {
+  width: 100%;
+  padding-left: 205px;
+  box-sizing: border-box;
+  height: 100%;
+  /* position: relative; */
+}
+</style>
